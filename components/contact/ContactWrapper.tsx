becc0e0e42bc590576d8d546c2a97a505ad47dda
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-01 12:16:04 +0545

    feature: initial layout

---
+++ components/contact/ContactWrapper.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from "@/components/contact/Landing";
+
+const ContactWrapper = () => {
+    return (
+        <div className="pt-16" >
+            <Landing />
+        </div>
+    );
+};
+
+export default ContactWrapper;

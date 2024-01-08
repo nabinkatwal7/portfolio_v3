d150876b44b681673d748fbfbfba416f06dda6f9
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-08 10:11:04 +0545

    font config

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div className="flex flex-col gap-10 w-[60%] m-auto justify-center pt-20" >
+            <div className="flex flex-col gap-2 justify-start text-left" >
+                <p className="text-4xl font-bold font-playfair" > John Doe </p>
+                <p className="text-3xl font-playfair" > Software Engineer </p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;

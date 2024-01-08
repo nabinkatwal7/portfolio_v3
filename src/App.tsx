35a5147a06d894697003c60d5295c1b6b669eb53
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-08 14:12:11 +0545

    completed first iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import './App.css'
+import "./App.css";
 import Home from "./components/Home.tsx";
 import Sunset from "./assets/sunset.svg";
 import Work from "./components/Work.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
   return (
-      <div>
-        <div className="relative min-h-screen">
-          <Home/>
-          <img className="absolute bottom-0 w-screen" src={Sunset} alt="sunset" />
-        </div>
-          <Work/>
+    <div className="pb-10">
+      <div className="relative min-h-screen">
+        <Home />
+        <img className="absolute bottom-0 w-screen" src={Sunset} alt="sunset" />
       </div>
-  )
+      <Work />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/data/resume.ts
+++ src/data/resume.ts
@@ -8,26 +8,21 @@ export type ResumeType = {
 export const ResumeData:ResumeType[] = [
     {
         id:"1",
-        date:"2021-2022",
-        title:"Software Engineer",
-        company:"Google",
+        date:"2023 August - Present",
+        title:"Frontend Engineer",
+        company:"AITC International",
     },
     {
         id:"2",
-        date:"2021-2022",
-        title:"Software Engineer",
-        company:"Google",
+        date:"2022 November - 2023 July (9 Months)",
+        title:"Frontend Engineer",
+        company:"Gyan Bazzar Online Private Limited",
     },
     {
         id:"3",
-        date:"2021-2022",
-        title:"Software Engineer",
-        company:"Google",
+        date:"2019-2022(3 Years)",
+        title:"Freelance Data Analyst",
+        company:"Upwork Inc.",
     },
-    {
-        id:"4",
-        date:"2021-2022",
-        title:"Software Engineer",
-        company:"Google",
-    },
+    
 ]

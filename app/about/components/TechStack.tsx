08c768e427ae1f60b6740a8a13ab8b14f0d77ecf
Nabin Katwal <nabinkatwal2000@gmail.com>
2025-03-21 15:27:47 +0545

    about us page

--- app/about/components/TechStack.tsx
+++ app/about/components/TechStack.tsx
@@ -1,5 +1,111 @@
+"use client";
+import TechComponent from "@/app/about/components/TechComponent";
+import CommonWrapper from "@/components/common/animation/CommonWrapper";
+import { Code, Tools, Web } from "@/data/TechStack";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { staggerChildren: 0.15 },
+  },
+};
+
 const TechStack = () => {
-  return <div>My Tech stack</div>;
+  return (
+    <CommonWrapper className="common-layout flex flex-col justify-center max-w-[1350px] gap-8 relative">
+      {/* Floating Water Effect */}
+      <div className="absolute inset-0 bg-blue-900/50 blur-3xl opacity-40 rounded-2xl"></div>
+
+      <div className="mx-auto px-4 md:px-8 flex justify-center flex-col gap-4 text-center lg:px-10">
+        <p className="text-base/7 font-semibold text-indigo-400 text-center">
+          My Tech Stack
+        </p>
+        <h2 className="text-4xl font-semibold tracking-tight text-pretty sm:text-5xl text-center">
+          Building the Future, One Line of Code at a Time
+        </h2>
+        <p>
+          Listen, a developer is only as good as their tools. You wouldn&apos;t
+          trust a lawyer without a briefcase, and you sure as hell
+          shouldn&apos;t trust a coder without a solid tech stack. From frontend
+          wizardry with React and Tailwind to backend muscle with Go and Rust,
+          I&apos;ve got the whole package. Need scalable apps? Boom—Next.js.
+          Need slick animations? Framer Motion&apos;s got it covered. Need
+          speed? Vite, baby. And let&apos;s not forget the essentials—Git,
+          Docker, AWS—because even the best code needs a rock-solid foundation.
+          So, if you&apos;re looking for top-tier tech, you just found it.
+          Let&apos;s build something legendary.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-4 justify-center">
+        <p className="text-3xl font-semibold tracking-tight text-pretty text-center">
+          Web Technologies
+        </p>
+        <div className="mx-auto w-full px-6 lg:px-8 relative">
+          <motion.div
+            className="-mx-6 grid grid-cols-2 gap-0.5 sm:mx-0 sm:rounded-2xl md:grid-cols-3 relative"
+            variants={containerVariants}
+            initial="hidden"
+            animate="show"
+          >
+            {Web.map((item) => (
+              <TechComponent
+                key={item.name}
+                name={item.name}
+                image={item.image}
+              />
+            ))}
+          </motion.div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 justify-center">
+        <p className="text-3xl font-semibold tracking-tight text-pretty text-center">
+          Programming
+        </p>
+        <div className="mx-auto w-full px-6 lg:px-8 relative">
+          <motion.div
+            className="-mx-6 grid grid-cols-2 gap-0.5 sm:mx-0 sm:rounded-2xl md:grid-cols-3 relative"
+            variants={containerVariants}
+            initial="hidden"
+            animate="show"
+          >
+            {Code.map((item) => (
+              <TechComponent
+                key={item.name}
+                name={item.name}
+                image={item.image}
+              />
+            ))}
+          </motion.div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 justify-center">
+        <p className="text-3xl font-semibold tracking-tight text-pretty text-center">
+          Tools
+        </p>
+        <div className="mx-auto w-full px-6 lg:px-8 relative">
+          <motion.div
+            className="-mx-6 grid grid-cols-2 gap-0.5 sm:mx-0 sm:rounded-2xl md:grid-cols-3 relative"
+            variants={containerVariants}
+            initial="hidden"
+            animate="show"
+          >
+            {Tools.map((item) => (
+              <TechComponent
+                key={item.name}
+                name={item.name}
+                image={item.image}
+              />
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </CommonWrapper>
+  );
 };
 
 export default TechStack;
---
+++ data/TechStack.ts
@@ -0,0 +1,216 @@
+export const Tools = [
+  {
+    name: "Github",
+    image: "/icons/github-dark.svg",
+  },
+  {
+    name: "Linux",
+    image: "/icons/linux.svg",
+  },
+  {
+    name: "Neovim",
+    image: "/icons/neovim.svg",
+  },
+  {
+    name: "VS Code",
+    image: "/icons/vscode.svg",
+  },
+  {
+    name: "Vim",
+    image: "/icons/vim.svg",
+  },
+  {
+    name: "Git",
+    image: "/icons/git.svg",
+  },
+  {
+    name: "Docker",
+    image: "/icons/docker.svg",
+  },
+  {
+    name: "AWS",
+    image: "/icons/aws_dark.svg",
+  },
+];
+
+export const Web = [
+  {
+    name: "Astro",
+    image: "/icons/web/astro_dark.svg",
+  },
+  {
+    name: "Babel",
+    image: "/icons/web/babel.svg",
+  },
+  {
+    name: "bun",
+    image: "/icons/web/bun.svg",
+  },
+  {
+    name: "Chartjs",
+    image: "/icons/web/chartjs.svg",
+  },
+  {
+    name: "CSS",
+    image: "/icons/web/css_old.svg",
+  },
+  {
+    name: "D3",
+    image: "/icons/web/D3.svg",
+  },
+  {
+    name: "ENV",
+    image: "/icons/web/dotenvx.svg",
+  },
+  {
+    name: "Express",
+    image: "/icons/web/expressjs_dark.svg",
+  },
+  {
+    name: "Figma",
+    image: "/icons/web/figma.svg",
+  },
+  {
+    name: "HTML",
+    image: "/icons/web/html5.svg",
+  },
+  {
+    name: "Javascript",
+    image: "/icons/web/javascript.svg",
+  },
+  {
+    name: "Jest",
+    image: "/icons/web/jest.svg",
+  },
+  {
+    name: "Material UI",
+    image: "/icons/web/materialui.svg",
+  },
+  {
+    name: "Framer",
+    image: "/icons/web/motion_dark.svg",
+  },
+  {
+    name: "Next JS",
+    image: "/icons/web/nextjs_icon_dark.svg",
+  },
+  {
+    name: "Node JS",
+    image: "/icons/web/nodejs.svg",
+  },
+  {
+    name: "NPM",
+    image: "/icons/web/npm.svg",
+  },
+  {
+    name: "PNPM",
+    image: "/icons/web/pnpm_dark.svg",
+  },
+  {
+    name: "PostCSS",
+    image: "/icons/web/postcss.svg",
+  },
+  {
+    name: "Prettier",
+    image: "/icons/web/prettier-icon-dark.svg",
+  },
+  {
+    name: "React",
+    image: "/icons/web/react_dark.svg",
+  },
+  {
+    name: "React Query",
+    image: "/icons/web/reactquery.svg",
+  },
+  {
+    name: "React Router",
+    image: "/icons/web/reactrouter.svg",
+  },
+  {
+    name: "Remix",
+    image: "/icons/web/remix_dark.svg",
+  },
+  {
+    name: "Shadcn",
+    image: "/icons/web/shadcn-ui_dark.svg",
+  },
+  {
+    name: "Svelte",
+    image: "/icons/web/svelte.svg",
+  },
+  {
+    name: "SWC",
+    image: "/icons/web/swc.svg",
+  },
+  {
+    name: "t3 Stack",
+    image: "/icons/web/t3-light.svg",
+  },
+  {
+    name: "TailwindCSS",
+    image: "/icons/web/tailwindcss.svg",
+  },
+  {
+    name: "TanStack",
+    image: "/icons/web/tanstack.svg",
+  },
+  {
+    name: "Three js",
+    image: "/icons/web/threejs-dark.svg",
+  },
+  {
+    name: "TypeScript",
+    image: "/icons/web/typescript.svg",
+  },
+  {
+    name: "Vercel",
+    image: "/icons/web/vercel_dark.svg",
+  },
+  {
+    name: "Vite",
+    image: "/icons/web/vitejs.svg",
+  },
+  {
+    name: "Vitest",
+    image: "/icons/web/vitest.svg",
+  },
+  {
+    name: "Zod",
+    image: "/icons/web/zod.svg",
+  },
+];
+
+export const Code = [
+  {
+    name: "Bash",
+    image: "/icons/code/bash_dark.svg",
+  },
+  {
+    name: "C++",
+    image: "/icons/code/c-plusplus.svg",
+  },
+  {
+    name: "C",
+    image: "/icons/code/c.svg",
+  },
+  {
+    name: "GO",
+    image: "/icons/code/golang_dark.svg",
+  },
+  {
+    name: "Lua",
+    image: "/icons/code/lua.svg",
+  },
+  {
+    name: "Markdown",
+    image: "/icons/code/markdown-dark.svg",
+  },
+  {
+    name: "Python",
+    image: "/icons/code/python.svg",
+  },
+  {
+    name: "Rust",
+    image: "/icons/code/rust_dark.svg",
+  },
+];

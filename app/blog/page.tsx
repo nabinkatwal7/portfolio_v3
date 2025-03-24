d7ac9ae3d61f440db0aab6834db4512a572395ac
Nabin Katwal <nabinkatwal2000@gmail.com>
2025-03-24 12:56:30 +0545

    feat: changes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,48 @@
-function Page() {
-    return (
-        <div>coming soon...</div>
-    );
+import Link from "next/link";
+import { client } from "../../tina/__generated__/client";
+
+export type PostConnection = {
+  postConnection: {
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+    totalCount: number;
+    edges: Array<{
+      cursor: string;
+      node: {
+        _sys: {
+          filename: string; // Added filename type based on usage
+        };
+        id: string;
+        __typename: string;
+        title: string;
+        body: Record<string, unknown>;
+      };
+    }>;
+  };
+};
+
+async function Page() {
+  // Type the response data using the PostConnection type
+  const { data } = await client.queries.postConnection();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        {data?.postConnection?.edges?.map((post) => (
+          <div key={post?.node?.id}>
+            <Link href={`/blog/${post?.node?._sys.filename}`}>
+              {post?.node?._sys.filename}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Page;

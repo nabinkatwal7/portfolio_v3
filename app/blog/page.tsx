4ea2fbcf07d581bd3fe49597850f0ed1824e0279
Nabin Katwal <nabinkatwal2000@gmail.com>
2025-03-24 16:12:02 +0545

    remove tinacms

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,48 +1,5 @@
-import Link from "next/link";
-import { client } from "../../tina/__generated__/client";
-
-export type PostConnection = {
-  postConnection: {
-    pageInfo: {
-      hasPreviousPage: boolean;
-      hasNextPage: boolean;
-      startCursor: string;
-      endCursor: string;
-    };
-    totalCount: number;
-    edges: Array<{
-      cursor: string;
-      node: {
-        _sys: {
-          filename: string; // Added filename type based on usage
-        };
-        id: string;
-        __typename: string;
-        title: string;
-        body: Record<string, unknown>;
-      };
-    }>;
-  };
+const Page = () => {
+  return <div>blog</div>;
 };
-
-async function Page() {
-  // Type the response data using the PostConnection type
-  const { data } = await client.queries.postConnection();
-
-  return (
-    <div>
-      <h1>Posts</h1>
-      <div>
-        {data?.postConnection?.edges?.map((post) => (
-          <div key={post?.node?.id}>
-            <Link href={`/blog/${post?.node?._sys.filename}`}>
-              {post?.node?._sys.filename}
-            </Link>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
 
 export default Page;

33e2fa9e79a61179b8f03ce5e7918b911d09f0ce
Nabin Katwal <nabinkatwal2000@gmail.com>
2025-03-24 12:07:17 +0545

    feat: addded projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,15 @@
+import Hero from "@/app/projects/components/Hero";
+import ProjectListing from "@/app/projects/components/ProjectListing";
+import CTA from "@/components/home/CTA";
 
 function Page() {
-    return (
-        <div>under construction
-
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-8">
+      <Hero />
+      <ProjectListing />
+      <CTA />
+    </div>
+  );
 }
 
 export default Page;

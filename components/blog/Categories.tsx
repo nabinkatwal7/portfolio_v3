30734ffdde8e51f4d4d9eb5fde1e556a96aa0eda
Nabin Katwal <nabinkatwal2000@gmail.com>
2025-03-25 11:53:28 +0545

    feat: blog layout

--- components/blog/Categories.tsx
+++ components/blog/Categories.tsx
@@ -6,11 +6,13 @@ type CategoriesProps = {
 
 export function Categories({ categories }: CategoriesProps) {
   return categories.map((category: any) => (
-    <span
+    <p
       key={category._id}
-      className="bg-cyan-50 rounded-full px-2 py-1 leading-none whitespace-nowrap text-sm font-semibold text-cyan-700"
+      className=" leading-none flex w-full content-center justify-center whitespace-nowrap text-sm font-semibold text-cyan-700"
     >
-      {category.title}
-    </span>
+      <span className="bg-cyan-50 px-2 py-1 rounded-full">
+        {category.title}
+      </span>
+    </p>
   ));
 }

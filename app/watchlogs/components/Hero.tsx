833a2c47912d28309850bbddad66272fb544ad95
Nabin Katwal <nabinkatwal2000@gmail.com>
2025-03-26 10:19:15 +0545

    feat: changes

---
+++ app/watchlogs/components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div className="common-layout">
+      <h1 className="text-4xl font-semibold tracking-tight text-pretty text-center sm:text-5xl">
+        Hang tight, This page is under construction
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;

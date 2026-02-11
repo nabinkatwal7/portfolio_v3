6baa6f6a47680f1a42eeabd8ada9690cf893603b
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2026-02-11 14:51:07 +0545

    Add Cloudflare Turnstile script and widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${syne.variable}`} suppressHydrationWarning>
+      <head>
+      <script
+  src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+  async
+  defer
+></script>
+      </head>
       <body
         className={`antialiased relative font-body text-[var(--color-text-main)] background-gradient transition-colors duration-300`}
       >
@@ -45,6 +52,13 @@ export default function RootLayout({
           <DynamicFontProvider />
           {children}
         </ThemeProvider>
+        <div
+  class="cf-turnstile"
+  data-sitekey="0x4AAAAAACaif-xi8CMpM_p1"
+  data-theme="light"
+  data-size="normal"
+  data-callback="onSuccess"
+></div>
       </body>
     </html>
   );
